485264551ea9a378c73fd774aacb65c78dcba7cd
MuhammadNofil <muhammadnofil1290@gmail.com>
2023-01-20 00:50:04 +0500

    push by nofil

--- index.js
+++ index.js
@@ -11,18 +11,17 @@ app.get('/',(req,res)=>{
     res.sendFile(filename,options)
 })
 
+let users=0;
 io.on('connection',(socket)=>{
     console.log("user is connected")
-    // to recive the message from frontend
-        socket.on('nofil',(data)=>{
-            console.log(data)
-        })
-    // sending message to client side 
-    setTimeout(()=>{
-        socket.emit("Nofil",{name:"nofil"})
-    },3000)
+    users++;
+    io.sockets.emit('newuser',{message:"wlcome to chat room"})
+    socket.broadcast.emit('newuser', {message: users +" user connected"})
+    
     socket.on("disconnect",()=>{
         console.log("user is disconnected")
+        users--;
+        socket.broadcast.emit('newuser', {message:users + "users disconnected"})
     })
 })
 
